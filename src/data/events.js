b8206823a48d7832c8e6c5ff396352e8ee14ce9a
Fidel Silak <fffsilakk@gmail.com>
2026-02-17 13:52:21 +0900

    first commit

---
+++ src/data/events.js
@@ -0,0 +1,15 @@
+// src/data/events.js
+
+export const events = [
+  {
+    slug: "event-1",
+    title: "The Logic & The Language", // Judul lebih editorial
+    desc: "Edisi perdana JayapuraJS: Mengupas tuntas fondasi Computational Thinking dan penguasaan Fundamental JavaScript untuk membangun karir tech yang solid di Papua.",
+    date: "14 Maret 2026",
+    time: "15:00 - 17:00 WIT",
+    location: "Jayapura, Papua",
+    link: "https://instagram.com/jayapurajs",
+    category: "Meetup",
+    image: "/meetup.jpeg",
+  },
+];
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,13 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			fontFamily: {
-				inter: ['Inter Variable', 'sans-serif']
-			}
-		},
-	},
-	darkMode: "class",
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ["Inter Variable", "sans-serif"],
+      },
+    },
+  },
+  darkMode: "class",
+  plugins: [],
+};
