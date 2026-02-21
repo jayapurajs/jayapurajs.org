94c93d3d7752ec0c392189e0e7c986893e73067e
Fidel Silak <fffsilakk@gmail.com>
2026-02-21 22:39:42 +0900

    feat: improve layout and styling for events, about, and navigation

--- src/data/events.js
+++ src/data/events.js
@@ -2,8 +2,8 @@
 
 export const events = [
   {
-    slug: "events-1",
-    title: "The Logic & The Language", // Judul lebih editorial
+    slug: "initial-community-meetup",
+    title: "Initial Meetup Jayapura JavaScript",
     desc: "Edisi perdana JayapuraJS: Mengupas tuntas fondasi Computational Thinking dan penguasaan Fundamental JavaScript untuk membangun karir tech yang solid di Papua.",
     date: "14 Maret 2026",
     time: "15:00 - 17:00 WIT",
